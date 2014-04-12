4421c2cafc93344b3d73aaae2d35e41887cad07e
Beablea <sofie.e.krog@gmail.com>
2014-04-12 16:38:38 +0200

    tryout

---
+++ Unitytryout/Assets/SquirrelMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquirrelMove : MonoBehaviour {
+
+	Vector3 velocity = Vector3.zero;
+	public Vector3 gravity; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//velocity += gravity; 
+		//transform.position += velocity * Time.deltaTime; 
+	}
+}
