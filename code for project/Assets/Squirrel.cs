665339408efcbb21f608bba7ed0cd29587238e7e
Nulz3n <pmalle12@student.aau.dk>
2014-04-12 15:27:53 +0200

    change in code.. not done atm

--- code for project/Assets/Squirrel.cs
+++ code for project/Assets/Squirrel.cs
@@ -5,9 +5,7 @@ using System.Collections;
 public class Squirrel : MonoBehaviour {
 
 	Vector3 velocity = Vector3.zero;
-	public Vector3 gravity;
-	public Vector3 jumpVelocity;
-	private float maxSpeed = 5f;
+	public float maxSpeed = 5f;
 	public float forwardSpeed = 1f;
 
 	bool didJump = false;
@@ -25,19 +23,11 @@ public class Squirrel : MonoBehaviour {
 
 	// physics engine update here
 	void FixedUpdate () {
-		velocity.x = forwardSpeed;
-		velocity += gravity*Time.deltaTime;
 
-		//the jumping physics
-		if (didJump == true) {
-			didJump = false;
-			if(velocity.y < 0){
-				velocity.y=0;}
-			velocity += jumpVelocity;
-		}
 
-		velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
 
-		transform.position += velocity *Time.deltaTime;
+	
 	}
+
+
 }
