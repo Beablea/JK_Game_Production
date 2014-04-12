a0d803296a1caf6e16d2a389a417187cec1142a5
Beablea <sofie.e.krog@gmail.com>
2014-04-12 17:05:28 +0200

    Birde added

--- Unitytryout/Assets/SquirrelMove.cs
+++ Unitytryout/Assets/SquirrelMove.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class SquirrelMove : MonoBehaviour {
-
+	public float speed = 1.0f; 
 	Vector3 velocity = Vector3.zero;
 	public Vector3 gravity; 
 	// Use this for initialization
@@ -12,6 +12,7 @@ public class SquirrelMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		rigidbody2D.AddForce(Vector2.right * speed); 
 		//velocity += gravity; 
 		//transform.position += velocity * Time.deltaTime; 
 	}
