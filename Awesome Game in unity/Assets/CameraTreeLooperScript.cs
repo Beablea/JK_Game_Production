3d2eb030fefea3bbad8cebeb52306a23b8c01980
Beablea <sofie.e.krog@gmail.com>
2014-04-13 16:38:12 +0200

    Version with all code together so far

---
+++ Awesome Game in unity/Assets/CameraTreeLooperScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTreeLooperScript : MonoBehaviour {
+
+	//
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log ("Triggered: " + collider.name);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Awesome Game in unity/Assets/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : CameraForward{
+	public GameObject birde; 
+	public Vector2 spawn_position; 
+	public float timer = 0.0f;
+	public float minLevelHeight = 0.0f; 
+	public float maxLevelHeight = 7.0f; 
+	public float minLevelWidth = 0.0f; 
+	public float maxLevelWidth = 10.0f;  
+	float spY; 
+	float spX;
+	CameraForward player ; 
+	
+	public void calculateSpawnPosition(){
+		float spawnX = Random.Range (minLevelWidth, maxLevelWidth); 
+		float spawnY = Random.Range(minLevelHeight, maxLevelHeight);
+		spX = spawnX + player.position.x; 
+		spY = spawnY + player.position.y;
+		spawn_position = new Vector2 (spX, spY);
+	}
+	public void spawn_birde(){
+		calculateSpawnPosition(); 
+		GameObject clone = (GameObject) Instantiate(birde, spawn_position, Quaternion.identity);
+		Destroy (clone, 5); 
+		
+	}
+	
+	// Use this for initialization
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		
+		if (timer > 1.5f ){
+			spawn_birde(); 
+			timer = 0.0f;
+		}
+	}
+}
