30e1874b0c7ac2b1bf10614e654914eb768fac2a
Beablea <sofie.e.krog@gmail.com>
2014-04-13 12:26:23 +0200

    spawn development

--- Unitytryout/Assets/Spawn_birde.cs
+++ Unitytryout/Assets/Spawn_birde.cs
@@ -5,18 +5,26 @@ public class Spawn_birde : MonoBehaviour {
 	public GameObject birde; 
 	public Vector2 spawn_position; 
 	public float timer = 0.0f; 
-	Random rng = new Random(); 
-	
-	
-	public void spawn_birde(){
-		Vector2 newspawnposition = Vector2.zero; 
-		Instantiate(birde, spawn_position, Quaternion.identity);
+	public int minLevelHeight = 0; 
+	public int maxLevelHeight = 10; 
+	public int minLevelWidth = 0; 
+	public int maxLevelWidth = 10; 
+
+	public void calculateSpawnPosition(){
+		int spawnX = Random.Range (minLevelWidth, maxLevelWidth); 
+		int spawnY = Random.Range(minLevelHeight, maxLevelHeight); 
+		spawn_position = new Vector2 (spawnX, spawnY);
 	}
+	public void spawn_birde(){
+		calculateSpawnPosition(); 
+		Instantiate(birde, spawn_position, Quaternion.identity); 
+
+	}
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime; 
