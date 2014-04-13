bd094f3527fa2dd47840794c52e5935ec09d0e99
CrappyBird <aqvist12@student.aau.dk>
2014-04-13 16:56:19 +0200

    commit stuff

--- CameraForward.cs
+++ CameraForward.cs
@@ -9,24 +9,24 @@ public class CameraForward : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		GameObject player_go = GameObject.FindGameObjectWithTag("Player");
-		
+
 		if(player_go == null) {
 			return;
 		}
-		
+
 		player = player_go.transform;
-		
+
 		offsetX = transform.position.x - player.position.x;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 		if(player != null) {
 			Vector2 pos = transform.position;
 			pos.x = player.position.x + offsetX;
 			transform.position = pos;
 		}
-	
+
 	}
 }
