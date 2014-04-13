d8b9a0c2f2edd54e129d577e997df8432e2d0e22
CrappyBird <aqvist12@student.aau.dk>
2014-04-13 15:04:53 +0200

    unity camera forward and trees box collider
    working on this still

---
+++ Camera forward and trees/Assets/CameraForward.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraForward : MonoBehaviour {
+
+	public float forwardSpeed = 0f; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		rigidbody2D.AddForce(Vector2.right * forwardSpeed);
+	
+	}
+}
