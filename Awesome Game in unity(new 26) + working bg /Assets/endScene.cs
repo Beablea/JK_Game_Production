441a86605c8ad5c393109509a699e376ffeb56e5
CrappyBird <aqvist12@student.aau.dk>
2014-04-25 18:57:30 +0200

    new release 26

---
+++ Awesome Game in unity(new 26) + working bg /Assets/endScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endScene : Squirrel {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			didJump = true;
+		if (didJump){
+				Application.LoadLevel("new");
+			}
+		}
+
+	}
+}
