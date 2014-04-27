a00900383d4cdb495c1801cc1d1b44f0c5caf88f
Beablea <sofie.e.krog@gmail.com>
2014-04-27 12:59:59 +0200

    tried to workout the highscore system

---
+++ Awesome Game in unity (new 27_4) kl.12.59/Assets/endScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endScene : Squirrel { 
+	public int HighScore = GetComponent<CounterScore>;  
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			didJump = true;
+		if (didJump){
+				Application.LoadLevel("new");
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Application.Quit();
+		}
+
+	}
+	public void OnGUI(){
+		GUI.Box (new Rect (450, 300, 150, 25), "High score: "+ HighScore);
+	}
+}
