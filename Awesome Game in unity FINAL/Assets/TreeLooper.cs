fb7c8658393722c3128a54b3df0280eeaac6d277
Beablea <sofie.e.krog@gmail.com>
2014-04-29 13:18:36 +0200

    delete and added comments

--- Awesome Game in unity FINAL/Assets/TreeLooper.cs
+++ Awesome Game in unity FINAL/Assets/TreeLooper.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class TreeLooper : MonoBehaviour {
+	//initializing variables 
 	public GameObject tree1; 
 	public GameObject tree2; 
 	public GameObject tree3; 
@@ -12,6 +13,7 @@ public class TreeLooper : MonoBehaviour {
 	public float minLevWidth = 1.0f; 
 	public float maxLevWidth = 35.0f;
 
+	//Calsulating the position in which the trees should spawn
 	public void calculateTreePosition(){
 		trees = new GameObject[] {tree1, tree2, tree3}; 
 		float distance= PlayerPrefs.GetFloat("distance");
@@ -19,11 +21,13 @@ public class TreeLooper : MonoBehaviour {
 		tree_position = treeVec;
 		
 	}
+	//Spawns a random tree from a array of the three trees 
 	public void spawn_tree(){
 		calculateTreePosition(); 
 		int spawnObjectIndex = Random.Range(0,trees.Length);
 		GameObject randPrefab = trees[spawnObjectIndex];
 		GameObject clonetree = (GameObject) Instantiate(randPrefab, tree_position, Quaternion.identity);
+		//destroying the trees after 12 seconds in the scene
 		Destroy (clonetree, 12);
 		
 	}
@@ -34,6 +38,7 @@ public class TreeLooper : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//making the trees spawn every second
 		time += Time.deltaTime;
 		if (time > 1.0f ){
 			spawn_tree(); 
